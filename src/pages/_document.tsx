d03502c10d177b36592d12b7f204e4a02b6f5476
DruMoDev <dev.morera@gmail.com>
2024-08-02 17:00:37 +0200

    Tipografia cambiada

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto+Slab:wght@100..900&family=Sevillana&display=swap" rel="stylesheet"/>
+
+      </Head>
       <body>
         <Main />
         <NextScript />
