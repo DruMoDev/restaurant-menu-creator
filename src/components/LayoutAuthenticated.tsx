160ff9ce5b06b4211f2a7681bd359ec0d94519dc
DruMoDev <dev.morera@gmail.com>
2024-08-11 21:01:52 +0200

    type updated

--- src/components/LayoutAuthenticated.tsx
+++ src/components/LayoutAuthenticated.tsx
@@ -1,7 +1,11 @@
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
 import SideNav from "./navs/SideNav";
 
-const LayoutAuthenticated = ({ children }): ReactElement => {
+const LayoutAuthenticated = ({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement => {
   return (
     <>
       <main className="min-h-screen container pt-24  flex flex-col mx-auto px-20 xl:max-w-[1700px] ">
